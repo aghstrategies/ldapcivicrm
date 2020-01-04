976fad339c4f30b20032153a0ff20ccaeb4738fc
Andrew Hunt <andrew@aghstrategies.com>
2020-01-03 20:02:50 -0500

    SSL params

--- bin/ldapcivicrm.php
+++ bin/ldapcivicrm.php
@@ -3,4 +3,18 @@ require "__FILE__/../vendor/autoload.php";
 use FreeDSx\Ldap\LdapServer;
 use Agh\RequestHandler;
 
-$server = (new LdapServer(['request_handler' => RequestHandler::class]))->run();
+function getSslDetails() {
+  require __DIR__ . '/../config/config.php';
+  return [
+    $ssl_cert_path,
+    $ssl_key_path,
+  ];
+}
+
+list($ssl_cert_path, $ssl_key_path) = getSslDetails();
+
+$server = (new LdapServer([
+  'request_handler' => RequestHandler::class,
+  'ssl_cert' => $ssl_cert_path,
+  'ssl_cert_key' => $ssl_key_path,
+]))->run();
