2cad17f80392f578481dc8f917bc3007a4b2e197
Andrew Hunt <andrew@aghstrategies.com>
2020-01-06 12:37:51 -0500

    Identify server

--- bin/ldapcivicrm.php
+++ bin/ldapcivicrm.php
@@ -17,4 +17,7 @@ $server = (new LdapServer([
   'request_handler' => RequestHandler::class,
   'ssl_cert' => $ssl_cert_path,
   'ssl_cert_key' => $ssl_key_path,
+  'dse_naming_contexts' => 'dc=ldap,dc=aghstrategies,dc=com',
+  'dse_vendor_name' => 'LDAP-CiviCRM',
+  'dse_vendor_version' => '1.0',
 ]))->run();
